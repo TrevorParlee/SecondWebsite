e698d3395deb29316b6ed08647a1536c39b9a9c4
Trevor Parlee <trevorparlee@gmail.com>
2021-02-23 12:53:45 -0700

    trials 2

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="\videos\video-4.mp4" autoPlay loop muted />
+      <video src="../../videos/video-4.mp4" autoPlay loop muted />
       {/* <img src="../images/img-5.jpg" alt="my set up" className="image" /> */}
       <h1>What are you waiting for?</h1>
       <div className="hero-btns">
